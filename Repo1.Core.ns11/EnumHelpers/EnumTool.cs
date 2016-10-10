2434a73f585e468e8a4be0637b2e9d11287b95b4
Peterson Salamat <petersonsalamat@gmail.com>
2016-10-10 15:42:44 +0800

    Added method for listing all values of an enum.

---
+++ Repo1.Core.ns11/EnumHelpers/EnumTool.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repo1.Core.ns11.EnumHelpers
+{
+    public class EnumTool
+    {
+        public static List<T> List<T>()
+        {
+            var list = new List<T>();
+
+            foreach (T item in Enum.GetValues(typeof(T)))
+                list.Add(item);
+
+            return list;
+        }
+    }
+}
